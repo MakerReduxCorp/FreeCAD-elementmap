c22911d5fec20db3e27ba84245a399dc80562d81
Pesc0 <pesco.conti@gmail.com>
2023-04-18 22:23:05 +0200

    move lazy to its own file

--- src/App/MappedName.h
+++ src/App/MappedName.h
@@ -33,64 +33,12 @@
 
 #include "ComplexGeoData.h"
 #include "IndexedName.h"
-
+#include "LazyClass.hpp"
 
 namespace Data
 {
 
-
-
-template <typename T>
-class Lazy {
-public:
-    Lazy() = default;
-
-    explicit Lazy(const T& resource) : handle(std::make_shared<T>(resource)), owner(true) {}
-    explicit Lazy(const std::shared_ptr<T>& resource) : handle(resource) {}
-
-    Lazy(const Lazy& other) : handle(other.handle), owner(false) {}
-    Lazy& operator=(const Lazy& other) 
-    {
-        handle = other.handle;
-        owner = false;
-    }
-
-    Lazy(Lazy&& other) noexcept : handle(std::move(other.handle)), owner(other.owner) {}
-    Lazy& operator=(Lazy&& other) noexcept
-    {
-        this->handle = std::move(other.handle);
-        this->owner = other.owner;
-        return *this;
-    }
-
-    ~Lazy() = default;
-
-    void createLocalCopy()
-    {
-        if (!owner)
-        {
-            handle.reset(std::make_shared<T>(*handle));
-            owner = true;
-        }
-    }
-
-    bool hasLocalCopy() const { return owner; }
-    
-    operator const T&() & const { return *handle; }
-    operator T&() &
-    {
-        createLocalCopy();
-        return *handle;
-    }
-
-private:
-    std::shared_ptr<T> handle;
-    bool owner = false;
-};
-
 typedef Lazy<std::string> LazyString;
-
-
 
 
 // NOLINTBEGIN(cppcoreguidelines-pro-bounds-pointer-arithmetic)
